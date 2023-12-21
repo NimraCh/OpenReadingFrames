4b3337cf25acb45634694ac04f40c9a6db8265d4
Nimra Chaudhary <nimrajaved926@gmail.com>
2023-12-21 10:21:38 +0500

    changes

--- orf_finder.py
+++ orf_finder.py
@@ -4,14 +4,35 @@ import sys
 
 #Your Code Here
 class ORFFinder:
-    def __init__
-    # Correctly initialize class
-
-    def read_fasta_file   # Implement code to read fasta file and return sequences
-
-    def find_longest_orf  # Implement code to find the longest DNA ORF in a given sequence
-
-    def process_sequences #Implement code to process all sequences and find the overall longest DNA ORF
+    def __init__ (self,file):
+        self.file=file
         pass
+# Implement code to read fasta file and return sequences
+    def read_fasta_file(file):
+        file=open(("SeqRandom.txt"),"r")
+        file=file.read()
+        file=file.rstrip(">")
+        file=file[1:]
+        file=[i.split("\n",1) for i in file]
+        file=[i[1].replace("\n","") for i in file]
+        return file
 
-def main # initialize to let various arguments pass
+ # Implement code to find the longest DNA ORF in a given sequence
+    
+    def find_longest_orf(seq):
+        seq=seq.upper()
+        start_codon="ATG"
+        stop_codons=["TAG","TAA","TGA"]
+        orfs=[]
+        for i in range(len(seq)):
+            if seq[i:i+3]==start_codon:
+                for j in range(i+3,len(seq),3):
+                    if seq[j:j+3] in stop_codons:
+                        orfs.append(seq[i:j+3])
+                    break
+            if orfs==[]:
+                return None
+            else:
+                return max(orfs,key=len)
+
+
